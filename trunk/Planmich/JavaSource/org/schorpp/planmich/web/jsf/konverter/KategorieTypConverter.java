2cec183cb1fa55244f76f62dc52e1eec0240cb97
goldie <goldie@39a672ce-f22e-0410-be28-ccd52d4524cc>
2007-05-08 16:11:11 +0000

    Initial import.

---
+++ trunk/Planmich/JavaSource/org/schorpp/planmich/web/jsf/konverter/KategorieTypConverter.java
@@ -0,0 +1,27 @@
+package org.schorpp.planmich.web.jsf.konverter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+
+import org.schorpp.planmich.domain.KategorieTyp;
+
+public class KategorieTypConverter implements Converter {
+
+	public Object getAsObject(FacesContext context, UIComponent component, String value)
+			throws ConverterException {
+		
+		try {
+			return KategorieTyp.valueOf(value);
+		} catch (IllegalArgumentException e) {
+			throw new ConverterException();
+		}
+	}
+
+	public String getAsString(FacesContext context, UIComponent component, Object value)
+			throws ConverterException {
+		return value.toString();
+	}
+
+}
